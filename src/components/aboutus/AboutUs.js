0489f8f729547f934f11e53fae19efc158ff57aa
SalahHammash <s.m.hammash@gmail.com>
2023-03-25 15:23:01 +0300

    salah work (About-Us) is done

--- src/components/aboutus/AboutUs.js
+++ src/components/aboutus/AboutUs.js
@@ -1,5 +1,18 @@
 import React from "react";
+import "../aboutus/aboutus.css"
+
 
 export default function AboutUs() {
-  return <div>AboutUs</div>;
+  return (
+    <div className="aboutUs-parent ">
+      <h3>ABOUT US</h3>
+      <p className="paragraph">We're LTUC ARENA . We exist to unite the conditioning* community.
+
+It's not our goals that unite us, but the things we do to achieve them. 
+Because although our training grounds and end goals might be different, sweat is our sport. And we're a team of individuals who know that to go further, we go together.. </p>
+
+    </div>
+
+  )
+
 }
