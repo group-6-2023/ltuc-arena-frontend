26dacbbee4eef3741546eb32b13439161b52d66c
amjad alsayyed <amgad.alsayyed@yahoo.com>
2023-03-26 18:44:14 +0300

    Auth0

--- src/components/AboutUs/AboutUs.js
+++ src/components/AboutUs/AboutUs.js
@@ -1,22 +1,18 @@
 import React from "react";
 import "./AboutUs.css";
 
-
 export default function AboutUs() {
   return (
     <div className="aboutUs-parent ">
       <h3>ABOUT US</h3>
       <p className="paragraph2">
-        We're LTUC ARENA . We exist to unite the conditioning* community.
+        We're LTUC ARENA . We exist to unite the conditioning community.
         <br />
         It's not our goals that unite us, but the things we do to achieve them.
         Because although our training grounds and end goals might be different,
         sweat is our sport. And we're a team of individuals who know that to go
         further, we go together..{" "}
       </p>
-
-    
-
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -4,13 +4,23 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter } from "react-router-dom";
+import { Auth0Provider } from "@auth0/auth0-react";
+
+const domain = process.env.REACT_APP_DOMAIN;
+const clientId = process.env.REACT_APP_CLIENT_ID;
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <Auth0Provider
+      domain={domain}
+      clientId={clientId}
+      redirectUri={window.location.origin}
+    >
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Auth0Provider>
   </React.StrictMode>
 );
 
