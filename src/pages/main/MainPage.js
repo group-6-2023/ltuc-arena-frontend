ce569fe6ce460b7d470d1234b66e93b57b856701
amjad alsayyed <amgad.alsayyed@yahoo.com>
2023-03-29 03:30:09 +0300

    exercise list

--- src/pages/main/MainPage.js
+++ src/pages/main/MainPage.js
@@ -4,21 +4,36 @@ import MotvationContent from "../../components/motvationcontent/MotvationContent
 import AboutUs from "../../components/AboutUs/AboutUs";
 import OurCards from "../../components/OurCards/OurCards";
 import BigRamy from "../../components/BigRamy/BigRamy";
+import CardsContanier from "../../components/cardsContanier/CardsContanier";
+import SearchExercises from "../../components/searchExercises/SearchExercises";
+import { useState } from "react";
 
 export default function MainPage() {
+  const [exercises, setExercises] = useState([]);
+  const [bodyPart, setBodyPart] = useState("all");
   return (
     <div>
-
       <PhotoCarousel />
 
       <MotvationContent />
 
       <BigRamy />
 
+      <SearchExercises
+        setExercises={setExercises}
+        bodyPart={bodyPart}
+        setBodyPart={setBodyPart}
+      />
+      <CardsContanier
+        isMain={true}
+        setExercises={setExercises}
+        exercises={exercises}
+        bodyPart={bodyPart}
+      />
+
       <AboutUs />
 
       <OurCards />
-
     </div>
   );
 }
