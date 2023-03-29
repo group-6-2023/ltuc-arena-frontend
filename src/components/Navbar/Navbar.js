22d1c4c1e7f374373186b1b2060cfe94ad3b52ee
amjad alsayyed <amgad.alsayyed@yahoo.com>
2023-03-30 00:13:07 +0300

    all done finailly insha alllah

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -32,14 +32,14 @@ function NavBar() {
             <Nav.Link href="#top" className="navLink">
               Main
             </Nav.Link>
+            <Nav.Link href="#explore" className="navLink">
+              Explore
+            </Nav.Link>
             <Nav.Link href="#AboutUs" className="navLink">
               About-Us
             </Nav.Link>
             <Nav.Link href="#OurTeam" className="navLink">
               Our-Team
-            </Nav.Link>
-            <Nav.Link href="#explore" className="navLink">
-              Explore
             </Nav.Link>
           </Nav>
         ) : (
